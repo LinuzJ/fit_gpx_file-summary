2e6d68925c29604b816f87554d6ab4380ff86473
Jern <linus.jern@nokia.com>
2021-07-12 10:50:45 +0300

    duration for .fit

--- create_summary.py
+++ create_summary.py
@@ -1,6 +1,11 @@
 import gpxpy
 import gpxpy.gpx
 from fitparse import FitFile
+from datetime import datetime
+
+def calculate_duration(start, end):
+    diff = end - start
+    return int(round(diff.total_seconds() / 60))
 
 def handle_fit(file):
     # Metrics we want to evaluate
@@ -15,20 +20,27 @@ def handle_fit(file):
     # collections of data
     hr = []
     speed = []
+    time_collection = []
     
     fitfile = FitFile(file)
     # Then go through all of the data entries in the file
     for data_message in fitfile.get_messages("record"):
         hr.append(data_message.get_value("heart_rate"))
         speed.append(data_message.get_value("speed"))
+        time_collection.append(data_message.get_value("timestamp"))
 
         if data_message.get_value("grade") and data_message.get_value("grade") > max_grade:
             max_grade = data_message.get_value("grade")
-    
+
+        if data_message.get_value("distance") and data_message.get_value("distance") > max_grade:
+            distance = data_message.get_value("distance")
+
+    time = calculate_duration(time_collection[0], time_collection[-1])
     max_hr = max(hr)
     avg_hr = sum(hr)/len(hr)
 
-    print(max_hr, avg_hr, max_grade)
+
+    print(time)
 
 
 
